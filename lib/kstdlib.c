6df8aef2aa8c5e32339db2896382135c0ec066fb
Wang Jing <wangjild@gmail.com>
2013-12-23 18:56:11 +0800

    为一些源码添加GPL协议

--- lib/kstdlib.c
+++ lib/kstdlib.c
@@ -1,5 +1,30 @@
-#include <kstdlib.h>
+/*
+ *  Copyright (C) 2013  Wang Jing <wangjild@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
+/*
+ *   Author:        wangjild <wangjild@gmail.com>
+ *   Github:        https://github.com/wangjild/OS_Hacking
+ *   File:          kstdlib.c
+ *   Lauguage:      c/cpp
+ *   Date:          13-12-08 19:21:04
+ *   Descripton:    kernel general library
+ */
+
+#include <kstdlib.h>
 uint8_t in_byte(uint16_t port) {
     uint8_t ret;
     __asm__ __volatile__ ("inb %1, %0" : "=a"(ret) : "dN" (port));
